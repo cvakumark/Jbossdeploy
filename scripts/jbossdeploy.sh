0a90cb8672e2faefa777971879df6cce7b1ffe45
cvakumark <cvakumark@users.noreply.github.com>
2017-04-07 12:20:20 +0800

    Update jbossdeploy.sh

--- scripts/jbossdeploy.sh
+++ scripts/jbossdeploy.sh
@@ -1,1 +1,1 @@
-/opt/jboss/bin/jboss-cli.sh --connect --controller=localhost:9999 --user=jbossdeploy --password=jbossdeploy@123 --commands="deploy /home/ubuntu/releases/hello-world.war --runtime-name=helloworld.war"
+/usr/local/share/jboss/bin/jboss-cli.sh --connect --controller=localhost:9999 --user=jbossdeploy --password=jbossdeploy123 --commands="deploy /home/ubuntu/releases/hello-world.war --runtime-name=helloworld.war"
