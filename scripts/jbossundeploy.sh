c48b5f1d4a9481de1490ba8942c3f3907dd7940a
cvakumark <cvakumark@users.noreply.github.com>
2017-04-07 12:20:56 +0800

    Update jbossundeploy.sh

--- scripts/jbossundeploy.sh
+++ scripts/jbossundeploy.sh
@@ -1,1 +1,1 @@
-/opt/jboss/bin/jboss-cli.sh --connect --controller=localhost:9999 --user=jbossdeploy --password=jbossdeploy@123 --commands="undeploy hello-world.war"
+/usr/local/share/jboss/bin/jboss-cli.sh --connect --controller=localhost:9999 --user=jbossdeploy --password=jbossdeploy123 --commands="undeploy hello-world.war"
